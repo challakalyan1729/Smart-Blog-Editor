83001e8a8ddcaf372d706cd172f26df9722e965c
Challa Kalyan <challakalyan1729@gmail.com>
2026-02-17 21:38:17 +0530

    Add postId state to editor store

--- src/store/useEditorStore.js
+++ src/store/useEditorStore.js
@@ -2,7 +2,9 @@ import { create } from 'zustand';
 
 export const useEditorStore = create((set) => ({
   editorState: null,
+  postId: null, // Track if we are editing an existing draft
   isSaving: false,
   setEditorState: (state) => set({ editorState: state }),
+  setPostId: (id) => set({ postId: id }),
   setIsSaving: (status) => set({ isSaving: status }),
 }));
